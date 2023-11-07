377caa388e5b814617177b652ffdb9c97db7a5ba
Erza <exposedmail@protonmail.ch>
2023-11-07 17:10:34 +0100

    Reduce amount of console output

--- index.js
+++ index.js
@@ -6,6 +6,11 @@ const axios = require("axios");
 const { faker } = require('@faker-js/faker');
 const url = "http://vortal.5v.pl/pliki/Updater/Aurera/evolunia.php";
 
+const counters = {
+	success: 0,
+	failure: 0,
+};
+
 async function sendPostRequest() {
   const data = {
     host: "evolunia.net:7171:1098",
@@ -19,12 +24,23 @@ async function sendPostRequest() {
     const response = await axios.post(url, data);
 	if (response.status != 200) {
 		console.log("Response:", response);
+		
+		if (++counters.failure == 10) {
+			process.exit();
+		}
 	} else {
-		console.log("Successfully planted junk data");
+		if (++counters.success == 100) {
+			console.log("Successfully planted junk data");
+
+			// Reset the counters if we make 100 successful requests without failing 10 times inbetween
+			counters.success = 0;
+			counters.failure = 0;
+		}
 	}
   } catch (error) {
     console.error("Error:", error);
   }
 }
 
+console.log("Starting to send requests");
 setInterval(sendPostRequest, 500);
