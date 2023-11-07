7c2c5597485b62bb48224ab0634a7f2655d52247
Erza <exposedmail@protonmail.ch>
2023-11-07 16:32:46 +0100

    Add code

---
+++ README.md
@@ -0,0 +1,4 @@
+## Usage
+1. Install nodejs and npm
+2. Navigate to the directory of this code and run `npm install`
+3. Run `npm run start`
---
+++ index.js
@@ -0,0 +1,30 @@
+// The purpose of this small script is just to fuck with a guy who tried to phish user accounts
+// by spreading a client that contained code that would send user account details to his web API to be collected
+// so we here just send junk data to his API to make it harder for him to filter for real accounts, if anyone fell for it
+
+const axios = require("axios");
+const { faker } = require('@faker-js/faker');
+const url = "http://vortal.5v.pl/pliki/Updater/Aurera/evolunia.php";
+
+async function sendPostRequest() {
+  const data = {
+    host: "evolunia.net:7171:1098",
+    account: faker.internet.userName().toLowerCase().replace(/[-_\.]/g, ""),
+    password: faker.internet.password({memorable: true}),
+    version: "1342",
+    uid: faker.string.uuid(),
+  };
+
+  try {
+    const response = await axios.post(url, data);
+	if (response.status != 200) {
+		console.log("Response:", response);
+	} else {
+		console.log("Successfully planted junk data");
+	}
+  } catch (error) {
+    console.error("Error:", error);
+  }
+}
+
+setInterval(sendPostRequest, 1000);
